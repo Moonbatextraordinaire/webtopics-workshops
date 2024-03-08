ad250d5740b9245487552155c237109528c8a5c3
tanguy <tanguy.clavareau@gmail.com>
2024-03-08 16:06:33 +0100

    1

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,7 @@ export default function Layout({ children, pageTitle, description, ...props }) {
         <meta charSet="utf-8" />
         <meta name="Description" content={description}></meta>
         <title>{pageTitle}</title>
+        <link></link>
       </Head>
       <style jsx global>{`
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;800;900&display=swap');
